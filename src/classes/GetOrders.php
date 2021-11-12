316721c33edc00d8ab4469bcfe02f42520959451
Veress Imre <veresi66@freemail.hu>
2021-11-12 09:41:52 +0100

    Első teszt G

--- src/classes/GetOrders.php
+++ src/classes/GetOrders.php
@@ -1,7 +1,15 @@
 <?php
 namespace App\classes;
 
+use Exception;
+use InvalidArgumentException;
+
 class GetOrders
 {
-    
+    public function __construct(string $argv, int $argc)
+    {
+        if ($argc != 2) {
+            throw new InvalidArgumentException('Ambiguous number of parameters!');
+        }
+    }
 }
